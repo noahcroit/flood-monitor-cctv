a023e4c46022b6af47986375d022a5f5cc0e80c3
noah <nattapong.w1004@gmail.com>
2023-03-24 04:04:50 +0700

    Update image buffering example

--- staff-gauge-reader/samples/image-buffer-opencv.py
+++ staff-gauge-reader/samples/image-buffer-opencv.py
@@ -11,7 +11,7 @@ def task_readframe(queue):
         while stream.isOpened():
             ret, frame = stream.read()
             queue.put(frame) # put frame into queue
-            time.sleep(0.01)  # simulate blocking delay
+            time.sleep(0.1)  # simulate blocking delay
         stream.release()
     except KeyboardInterrupt:
         pass
@@ -22,9 +22,20 @@ def task_display(queue):
             if not queue.empty():
                 frame = queue.get()
                 frame = cv2.resize(frame, (960, 540))
+                time.sleep(0.1)  # simulate blocking delay
                 cv2.imshow("frame", frame)
             # Press Q on keyboard to  exit
             cv2.waitKey(10)
+
+    except KeyboardInterrupt:
+        cv2.destroyAllWindows()
+
+def task_isqueuefull(queue):
+    try:
+        while True:
+            print("q size = ")
+            print(queue.qsize())
+            time.sleep(1)
 
     except KeyboardInterrupt:
         cv2.destroyAllWindows()
@@ -36,10 +47,13 @@ if __name__ == "__main__":
     # config tasks
     t1 = threading.Thread(target=task_readframe, args=(queue,))
     t2 = threading.Thread(target=task_display, args=(queue,))
+    t3 = threading.Thread(target=task_isqueuefull, args=(queue,))
     
     # start tasks
     t1.start()
     t2.start()
+    t3.start()
     # wait for all threads to finish
     t1.join()
     t2.join()
+    t3.join()
