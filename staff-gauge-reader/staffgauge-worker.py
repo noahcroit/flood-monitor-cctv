50858e7c47c58c839e6788a813a71a3d969307b3
noah <nattapong.w1004@gmail.com>
2023-05-08 15:01:25 +0700

    update staffgauge worker : add thread monitor in main, keyboard exception

--- staff-gauge-reader/staffgauge-worker.py
+++ staff-gauge-reader/staffgauge-worker.py
@@ -20,14 +20,14 @@ def task_snapshot(queue_snapshot):
     stream = cv2.VideoCapture(source)
     try:
         # looping
-        while stream.isOpened():
+        while stream.isOpened() and snapshot_isrun:
             ret, frame = stream.read()
             queue_snapshot.put(frame) # put frame into queue
             time.sleep(1)    # simulate blocking delay
         stream.release()
         snapshot_isrun = False
-    except KeyboardInterrupt:
-        print("stop snapshot task!")
+    except:
+        print("something wrong in task snapshot!")
         stream.release()
         snapshot_isrun = False
         
@@ -36,44 +36,47 @@ def task_overlay(queue_roi):
 
     # looping
     while snapshot_isrun:
-        if not queue_roi.empty():
-            dict_roi = queue_roi.get()
-            frame = dict_roi['frame']
-            obj_class = dict_roi['class']
-            x1 = dict_roi['x1']
-            y1 = dict_roi['y1']
-            x2 = dict_roi['x2']
-            y2 = dict_roi['y2']
-            color = dict_roi['color']
-            print(obj_class)
-            if not obj_class is None:
-                for i in range(len(obj_class)):
-                    # draw a bounding box rectangle and label on the frame
-                    cv2.rectangle(frame, (x1[i], y1[i]), (x2[i], y2[i]), [102, 220, 225], 2)
-                    #text = "{}: {:.4f}".format(obj_class[i], confidences[i])
-                    text = "{}".format(obj_class[i])
-                    cv2.putText(frame, text, (x1[i], y1[i] - 5), cv2.FONT_HERSHEY_SIMPLEX, 1.5, [102, 220, 225], 5)
+        try:
+            if not queue_roi.empty():
+                dict_roi = queue_roi.get()
+                frame = dict_roi['frame']
+                obj_class = dict_roi['class']
+                x1 = dict_roi['x1']
+                y1 = dict_roi['y1']
+                x2 = dict_roi['x2']
+                y2 = dict_roi['y2']
+                color = dict_roi['color']
+                print(obj_class)
+                if not obj_class is None:
+                    for i in range(len(obj_class)):
+                        # draw a bounding box rectangle and label on the frame
+                        cv2.rectangle(frame, (x1[i], y1[i]), (x2[i], y2[i]), [102, 220, 225], 2)
+                        #text = "{}: {:.4f}".format(obj_class[i], confidences[i])
+                        text = "{}".format(obj_class[i])
+                        cv2.putText(frame, text, (x1[i], y1[i] - 5), cv2.FONT_HERSHEY_SIMPLEX, 1.5, [102, 220, 225], 5)
 
-                # resize image frame for 1/2
-                w_resize = int(frame.shape[1] * 0.5)
-                h_resize = int(frame.shape[0] * 0.5)
-                dim = (w_resize, h_resize)
-                frame_resize = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
-                
-                # Using cv2.putText() method
-                # font
-                font = cv2.FONT_HERSHEY_SIMPLEX
-                org = (50, 50)
-                fontScale = 1
-                color = (255, 255, 255)
-                thickness = 2
-                level = dict_roi['level']
-                cv2.putText(frame_resize, 'waterlevel={}'.format(level), org, font, fontScale, color, thickness, cv2.LINE_AA)
+                    # resize image frame for 1/2
+                    w_resize = int(frame.shape[1] * 0.5)
+                    h_resize = int(frame.shape[0] * 0.5)
+                    dim = (w_resize, h_resize)
+                    frame_resize = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
+                    
+                    # Using cv2.putText() method
+                    # font
+                    font = cv2.FONT_HERSHEY_SIMPLEX
+                    org = (50, 50)
+                    fontScale = 1
+                    color = (255, 255, 255)
+                    thickness = 2
+                    level = dict_roi['level']
+                    cv2.putText(frame_resize, 'waterlevel={}'.format(level), org, font, fontScale, color, thickness, cv2.LINE_AA)
 
-                # draw overlay
-                cv2.imshow("output frame", frame_resize)
-                cv2.waitKey(1000)
-        time.sleep(0.1)
+                    # draw overlay
+                    cv2.imshow("output frame", frame_resize)
+                    cv2.waitKey(1000)
+            time.sleep(0.1)
+        except:
+            print("something is wrong in task overlay")
     cv2.destroyAllWindows()
 
 def task_find_roi(queue_in, queue_out):
@@ -108,127 +111,130 @@ def task_find_roi(queue_in, queue_out):
 
     # looping for YOLO
     while snapshot_isrun:
-        # read image frame from queue
-        frame_ready = False
-        if not queue_in.empty():
-            # read input buffer
-            frame_in = queue_in.get()
-            if not frame_in is None:
-                frame_ready = True
+        try:
+            # read image frame from queue
+            frame_ready = False
+            if not queue_in.empty():
+                # read input buffer
+                frame_in = queue_in.get()
+                if not frame_in is None:
+                    frame_ready = True
 
-        # processing
-        # YOLO object detection
-        if frame_ready:  
-            # "frame" needs to be opencv's image-dtype
-            # Input Image Configuration
-            print("[INFO] accessing image ...")
-            (H, W) = frame_in.shape[:2]
-        
-            # construct a blob from the input frame and then perform a forward
-            # pass of the YOLO object detector, giving us our bounding boxes
-            # and associated probabilities
-            blob = cv2.dnn.blobFromImage(frame_in, 1 / 255.0, (416, 416), swapRB=True, crop=False)
-            net.setInput(blob)
-
-            layerOutputs = net.forward(ln)
-
-            # initialize our lists of detected bounding boxes, confidences,
-            # and class IDs, respectively
-            boxes = []
-            confidences = []
-            classIDs = []
-
-            # loop over each of the layer outputs
-            for output in layerOutputs:
-                # loop over each of the detections
-                for detection in output:
-                    # extract the class ID and confidence (i.e., probability)
-                    # of the current object detection
-                    scores = detection[5:]
-                    classID = np.argmax(scores)
-                    confidence = scores[classID]
-
-                    # filter out weak predictions by ensuring the detected
-                    # probability is greater than the minimum probability
-                    if confidence > user_confidence:
-                        # scale the bounding box coordinates back relative to
-                        # the size of the image, keeping in mind that YOLO
-                        # actually returns the center (x, y)-coordinates of
-                        # the bounding box followed by the boxes' width and
-                        # height
-                        box = detection[0:4] * np.array([W, H, W, H])
-                        (centerX, centerY, width, height) = box.astype("int")
-
-                        # use the center (x, y)-coordinates to derive the top
-                        # and and left corner of the bounding box
-                        x = int(centerX - (width / 2))
-                        y = int(centerY - (height / 2))
-
-                        # update our list of bounding box coordinates,
-                        # confidences, and class IDs
-                        boxes.append([x, y, int(width), int(height)])
-                        confidences.append(float(confidence))
-                        classIDs.append(classID)
-
-            # apply non-maxima suppression to suppress weak, overlapping
-            # bounding boxes
-            idxs = cv2.dnn.NMSBoxes(boxes, confidences, user_confidence, user_threshold)
-
-            output_class = None
-            pos_x = None
-            pos_y = None
-            # ensure at least one detection exists
-            if len(idxs) > 0:
-                output_class = []
-                pos_x1 = []
-                pos_y1 = []
-                pos_x2 = []
-                pos_y2 = []
-                classes_color = []
-
-                # loop over the indexes we are keeping
-                for i in idxs.flatten():
-                    # extract the bounding box coordinates
-                    (x1, y1) = (boxes[i][0], boxes[i][1])
-                    (w, h) = (boxes[i][2], boxes[i][3])
-                    x2 = x1 + w
-                    y2 = y1 + h
-                    # output the result with this format
-                    # output_class = [output_class[0], output_class[1], output_class[2], ...]
-                    # [x1[0], x1[1], x1[2], ...]
-                    # [y1[0], y1[1], y1[2], ...]
-                    # [x2[0], x2[1], x2[2], ...]
-                    # [y2[0], y2[1], y2[2], ...]
-                    color = [int(c) for c in CLASS_COLORS[classIDs[i]]]
-                    output_class.append(LABELS[classIDs[i]])
-                    pos_x1.append(x1)
-                    pos_y1.append(y1)
-                    pos_x2.append(x2)
-                    pos_y2.append(y2)
-                    classes_color.append(color)
+            # processing
+            # YOLO object detection
+            if frame_ready:  
+                # "frame" needs to be opencv's image-dtype
+                # Input Image Configuration
+                print("[INFO] accessing image ...")
+                (H, W) = frame_in.shape[:2]
             
-            # write to output buffer after finished the process
-            print("Yolo finished")
+                # construct a blob from the input frame and then perform a forward
+                # pass of the YOLO object detector, giving us our bounding boxes
+                # and associated probabilities
+                blob = cv2.dnn.blobFromImage(frame_in, 1 / 255.0, (416, 416), swapRB=True, crop=False)
+                net.setInput(blob)
 
-            # waterlevel calculation
-            if not output_class is None:
-                level = measure_waterlevel(frame_in, pos_x1[0], pos_x2[0], pos_y1[0], pos_y2[0])
-                print("level={}".format(level))
+                layerOutputs = net.forward(ln)
 
-            dict_output = {"frame":frame_in, 
-                           "class":output_class, 
-                           "x1":pos_x1, 
-                           "y1":pos_y1, 
-                           "x2":pos_x2, 
-                           "y2":pos_y2,
-                           "color":classes_color,
-                           "level":level
-                           }
-            queue_out.put(dict_output)
-        else:
-            print("frame=None")
+                # initialize our lists of detected bounding boxes, confidences,
+                # and class IDs, respectively
+                boxes = []
+                confidences = []
+                classIDs = []
 
-        time.sleep(0.1)
+                # loop over each of the layer outputs
+                for output in layerOutputs:
+                    # loop over each of the detections
+                    for detection in output:
+                        # extract the class ID and confidence (i.e., probability)
+                        # of the current object detection
+                        scores = detection[5:]
+                        classID = np.argmax(scores)
+                        confidence = scores[classID]
+
+                        # filter out weak predictions by ensuring the detected
+                        # probability is greater than the minimum probability
+                        if confidence > user_confidence:
+                            # scale the bounding box coordinates back relative to
+                            # the size of the image, keeping in mind that YOLO
+                            # actually returns the center (x, y)-coordinates of
+                            # the bounding box followed by the boxes' width and
+                            # height
+                            box = detection[0:4] * np.array([W, H, W, H])
+                            (centerX, centerY, width, height) = box.astype("int")
+
+                            # use the center (x, y)-coordinates to derive the top
+                            # and and left corner of the bounding box
+                            x = int(centerX - (width / 2))
+                            y = int(centerY - (height / 2))
+
+                            # update our list of bounding box coordinates,
+                            # confidences, and class IDs
+                            boxes.append([x, y, int(width), int(height)])
+                            confidences.append(float(confidence))
+                            classIDs.append(classID)
+
+                # apply non-maxima suppression to suppress weak, overlapping
+                # bounding boxes
+                idxs = cv2.dnn.NMSBoxes(boxes, confidences, user_confidence, user_threshold)
+
+                output_class = None
+                pos_x = None
+                pos_y = None
+                # ensure at least one detection exists
+                if len(idxs) > 0:
+                    output_class = []
+                    pos_x1 = []
+                    pos_y1 = []
+                    pos_x2 = []
+                    pos_y2 = []
+                    classes_color = []
+
+                    # loop over the indexes we are keeping
+                    for i in idxs.flatten():
+                        # extract the bounding box coordinates
+                        (x1, y1) = (boxes[i][0], boxes[i][1])
+                        (w, h) = (boxes[i][2], boxes[i][3])
+                        x2 = x1 + w
+                        y2 = y1 + h
+                        # output the result with this format
+                        # output_class = [output_class[0], output_class[1], output_class[2], ...]
+                        # [x1[0], x1[1], x1[2], ...]
+                        # [y1[0], y1[1], y1[2], ...]
+                        # [x2[0], x2[1], x2[2], ...]
+                        # [y2[0], y2[1], y2[2], ...]
+                        color = [int(c) for c in CLASS_COLORS[classIDs[i]]]
+                        output_class.append(LABELS[classIDs[i]])
+                        pos_x1.append(x1)
+                        pos_y1.append(y1)
+                        pos_x2.append(x2)
+                        pos_y2.append(y2)
+                        classes_color.append(color)
+                
+                # write to output buffer after finished the process
+                print("Yolo finished")
+
+                # waterlevel calculation
+                if not output_class is None:
+                    level = measure_waterlevel(frame_in, pos_x1[0], pos_x2[0], pos_y1[0], pos_y2[0])
+                    print("level={}".format(level))
+
+                dict_output = {"frame":frame_in, 
+                               "class":output_class, 
+                               "x1":pos_x1, 
+                               "y1":pos_y1, 
+                               "x2":pos_x2, 
+                               "y2":pos_y2,
+                               "color":classes_color,
+                               "level":level
+                               }
+                queue_out.put(dict_output)
+            else:
+                print("frame=None")
+
+            time.sleep(0.1)
+        except:
+            print("something wrong in task YOLO")
 
 def measure_waterlevel(img, x1, x2, y1, y2):
     
@@ -309,9 +315,21 @@ if __name__ == "__main__":
     t3.start()
 
     # wait for all threads to finish
-    while t1.is_alive():
-        t1.join(1)
-        t2.join(1)
-        t3.join(1)
+    while snapshot_isrun:
+        try:
+            if not t1.is_alive():
+                print("snapshot task is dead. stop worker...")
+                snapshot_isrun = False
+            if not t2.is_alive():
+                print("restart task find roi")
+                t2.start()
+            if not t3.is_alive():
+                print("restart task overlay")
+                t3.start()
 
+            time.sleep(5)
+
+        except KeyboardInterrupt:
+            print("main thread interrupted. Stop snaptshot")
+            snapshot_isrun = False
 
