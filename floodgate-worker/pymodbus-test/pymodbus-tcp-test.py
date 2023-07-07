263aa15877f3674bc02de08f014cba21b68f5f88
noah <nattapong.w1004@gmail.com>
2023-07-08 01:36:55 +0700

    Add pymodbus test script

---
+++ floodgate-worker/pymodbus-test/pymodbus-tcp-test.py
@@ -0,0 +1,48 @@
+import time
+from pymodbus.client import ModbusTcpClient
+
+
+
+def test_coil(mbclient):
+    print("test write Coils")
+    try:
+        mbclient.write_coils(address=160, values=[1, 1, 0, 0, 0, 0, 0, 1], slave=0)
+        time.sleep(1)
+    except Exception as e:
+        print(e)
+
+    print("test read Coils")
+    try:
+        response = mbclient.read_coils(address=160, count=8, slave=0)
+        time.sleep(1)
+        print("response={}".format(response.bits))
+    except Exception as e:
+        print(e)
+
+
+def test_hold_register(mbclient):
+    print("test read HR")
+    try:
+        time.sleep(1)
+        response = mbclient.read_holding_registers(address=14000, count=1, slave=0)
+        print(response.registers)
+    except Exception as e:
+        print(e)
+
+
+
+def main():
+    print("pymodbus TCP testing...")
+    time.sleep(1)
+
+    client = ModbusTcpClient('127.0.0.1')
+    client.connect()
+    test_coil(client)
+    test_hold_register(client)
+    print("test end")
+
+
+
+
+if __name__ == '__main__':
+    main()
